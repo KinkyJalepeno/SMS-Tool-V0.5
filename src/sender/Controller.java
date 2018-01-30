f4aa5f05b3a126054b1d06a339d49f1dd859e7d5
Dave.Graham <wolfhammer@live.co.uk>
2018-01-30 13:29:08 +0000

    Got login working with server status check updates reflected in the UI.

--- src/sender/Controller.java
+++ src/sender/Controller.java
@@ -1,9 +1,19 @@
 package sender;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.paint.Color;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
+
+import java.awt.*;
+import java.io.*;
+import java.net.Socket;
+import java.util.Scanner;
 
 public class Controller {
 
@@ -18,6 +28,9 @@ public class Controller {
     @FXML private Button queryMast;
     @FXML private Button FlushMast;
     @FXML private Button clear;
+    @FXML private Button runServer;
+    @FXML private Button pauseServer;
+
 
     @FXML private TextField ipAddress;
     @FXML private TextField passWord;
@@ -27,5 +40,59 @@ public class Controller {
 
     @FXML private TextArea console;
 
+    @FXML private Label connStatus;
+    @FXML private Label serverStatus;
 
+
+
+
+
+
+    public void button(ActionEvent connect) throws IOException {
+
+    authenticate();
+
+    }// end button method
+
+    public void authenticate() throws IOException {
+        Socket s = new Socket(ipAddress.getText(), 63333);
+
+        PrintWriter p = new PrintWriter(s.getOutputStream(), true);
+        BufferedReader bufRd = new BufferedReader(new InputStreamReader(s.getInputStream()));
+
+        p.println("{\"method\":\"authentication\",\"server_password\":\"" + passWord.getText() + "\"}");
+        String response = bufRd.readLine();
+        p.println("");
+        response = bufRd.readLine();
+        console.appendText(response + "\n");
+
+        if (response.equals("SMS server connected")){
+            connStatus.setText("Connected");
+            connStatus.setTextFill(Color.GREEN);
+        }
+    checkStatus(s);
+    }
+
+    public void checkStatus(Socket s)throws IOException{
+
+        PrintWriter p = new PrintWriter(s.getOutputStream(), true);
+        BufferedReader bufRd = new BufferedReader(new InputStreamReader(s.getInputStream()));
+
+        p.println("{\"method\":\"get_server_status\"}");
+        String response = bufRd.readLine();
+
+        //System.out.println(response +"\n");
+
+        Object obj = JSONValue.parse(response);
+        JSONObject jsonObject = (JSONObject) obj;
+
+        String status = (String) jsonObject.get("server_currently_status");
+
+        serverStatus.setText(status);
+        if (status.equals("Running")) {
+            serverStatus.setTextFill(Color.GREEN);
+        }
+        else serverStatus.setTextFill(Color.RED);
+
+    }//end checkStatus
 }//end class
