59d5854f9694cc361a85378e5b531143fc243023
Kinkyjalepeno <kinkyjalepeno@gmail.com>
2018-01-30 18:34:07 +0000

    Figured out some serious shit about what to do where ! Main code is now in Main.java leaving Controller to watch for buttons and call methods in Main.java

--- src/sender/Controller.java
+++ src/sender/Controller.java
@@ -6,14 +6,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
-import javafx.scene.paint.Color;
-import org.json.simple.JSONObject;
-import org.json.simple.JSONValue;
-
-import java.awt.*;
 import java.io.*;
-import java.net.Socket;
-import java.util.Scanner;
 
 public class Controller {
 
@@ -32,67 +25,22 @@ public class Controller {
     @FXML public Button pauseServer;
 
 
-    @FXML public TextField ipAddress;
-    @FXML public TextField passWord;
+    @FXML public static TextField ipAddress;
+    @FXML public static TextField passWord;
     @FXML public TextField card;
     @FXML public TextField card2;
     @FXML public TextField port;
 
-    @FXML public TextArea console;
+    @FXML public static TextArea console;
 
-    @FXML public Label connStatus;
-    @FXML public Label serverStatus;
-
-
-
-
-
+    @FXML public static Label connStatus;
+    @FXML public static Label serverStatus;
 
     public void button(ActionEvent connect) throws IOException {
 
-    authenticate();
+    sender.Main.connect();
 
     }// end button method
 
-    public void authenticate() throws IOException {
-        Socket s = new Socket(ipAddress.getText(), 63333);
 
-        PrintWriter p = new PrintWriter(s.getOutputStream(), true);
-        BufferedReader bufRd = new BufferedReader(new InputStreamReader(s.getInputStream()));
-
-        p.println("{\"method\":\"authentication\",\"server_password\":\"" + passWord.getText() + "\"}");
-        String response = bufRd.readLine();
-        p.println("");
-        response = bufRd.readLine();
-        console.appendText(response + "\n");
-
-        if (response.equals("SMS server connected")){
-            connStatus.setText("Connected");
-            connStatus.setTextFill(Color.GREEN);
-        }
-    checkStatus(s);
-    }
-
-    public void checkStatus(Socket s)throws IOException{
-
-        PrintWriter p = new PrintWriter(s.getOutputStream(), true);
-        BufferedReader bufRd = new BufferedReader(new InputStreamReader(s.getInputStream()));
-
-        p.println("{\"method\":\"get_server_status\"}");
-        String response = bufRd.readLine();
-
-        //System.out.println(response +"\n");
-
-        Object obj = JSONValue.parse(response);
-        JSONObject jsonObject = (JSONObject) obj;
-
-        String status = (String) jsonObject.get("server_currently_status");
-
-        serverStatus.setText(status);
-        if (status.equals("Running")) {
-            serverStatus.setTextFill(Color.GREEN);
-        }
-        else serverStatus.setTextFill(Color.RED);
-
-    }//end checkStatus
 }//end class
--- src/sender/Main.java
+++ src/sender/Main.java
@@ -4,7 +4,16 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
+
+import java.io.*;
+import java.net.Socket;
+
+import static sender.Controller.*;
+
 
 public class Main extends Application {
 
@@ -22,4 +31,53 @@ public class Main extends Application {
     public static void main(String[] args) {
         launch(args);
     }
-}
+
+    public static void connect()throws IOException {
+
+
+        Socket s = new Socket(ipAddress.getText(), 63333);
+
+        PrintWriter p = new PrintWriter(s.getOutputStream(), true);
+        BufferedReader bufRd = new BufferedReader(new InputStreamReader(s.getInputStream()));
+
+        p.println("{\"method\":\"authentication\",\"server_password\":\"" + passWord.getText() + "\"}");
+        String response = bufRd.readLine();
+        p.println("");
+        response = bufRd.readLine();
+        console.appendText(response + "\n");
+
+        if (response.equals("SMS server connected")){
+            connStatus.setText("Connected");
+            connStatus.setTextFill(Color.GREEN);
+        }
+        checkStatus(s);
+
+
+    }
+
+    public static void checkStatus(Socket s)throws IOException{
+
+        PrintWriter p = new PrintWriter(s.getOutputStream(), true);
+        BufferedReader bufRd = new BufferedReader(new InputStreamReader(s.getInputStream()));
+
+        p.println("{\"method\":\"get_server_status\"}");
+        String response = bufRd.readLine();
+
+        //System.out.println(response +"\n");
+
+        Object obj = JSONValue.parse(response);
+        JSONObject jsonObject = (JSONObject) obj;
+
+        String status = (String) jsonObject.get("server_currently_status");
+
+        serverStatus.setText(status);
+        if (status.equals("Running")) {
+            serverStatus.setTextFill(Color.GREEN);
+        }
+        else serverStatus.setTextFill(Color.RED);
+    }
+
+
+
+}//end class
+
