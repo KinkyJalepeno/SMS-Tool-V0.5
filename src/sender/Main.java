d9a14f35083dc4a75d3dca7209277820a60362ba
Dave.Graham <wolfhammer@live.co.uk>
2018-02-07 13:36:28 +0000

    Basic app finished !!!!!

--- src/sender/Main.java
+++ src/sender/Main.java
@@ -11,34 +11,10 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("GUI.fxml"));
-        primaryStage.setTitle("Dave's SMS Sender");
+        primaryStage.setTitle("Kinky Jalepenos' SMS Sender @2018");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
 
-
-
-//    public static void pauseServer(Socket s) throws Exception{
-//
-//        PrintWriter p = new PrintWriter(s.getOutputStream(), true);
-//        BufferedReader bufRd = new BufferedReader(new InputStreamReader(s.getInputStream()));
-//
-//        p.println("{ \"method\":\"pause_server\" }");
-//        String response = bufRd.readLine();
-//
-//    }// end pauseServer
-//
-//    public static void runServer(Socket s) throws Exception{
-//
-//        PrintWriter p = new PrintWriter(s.getOutputStream(), true);
-//        BufferedReader bufRd = new BufferedReader(new InputStreamReader(s.getInputStream()));
-//
-//        p.println("{ \"method\":\"run_server\" }");
-//        String response = bufRd.readLine();
-//
-//    }//end runServer
-//
-//
-//    }//end setRand
 }//end main class
 
